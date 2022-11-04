daf79e67ff622bb0580205f8a979df54691da7ee
xLytje <115148197+xLytje@users.noreply.github.com>
2022-11-04 14:15:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+(function () {
+    const items = [
+        '⭕',
+        '❌',
+        '🔺',
+        '🍌',
+    ];
+
+
+    const doors = document.querySelectorAll('.door');
+    
+    document.querySelector('#spinner').addEventListener('click', spin);
+  
+    function init(firstInit = true, groups = 1, duration = 1) {
+      for (const door of doors) {
+        if (firstInit) {
+          door.dataset.spinned = '0';
+        } else if (door.dataset.spinned === '1') {
+          return;
+        }
+  
+        const boxes = door.querySelector('.boxes');
+        const boxesClone = boxes.cloneNode(false);
+        const pool = ['❓'];
+  
+        if (!firstInit) {
+          const arr = [];
+          for (let n = 0; n < (groups > 0 ? groups : 1); n++) {
+            arr.push(...items);
+          }
+          pool.push(...shuffle(arr))
+        }
+  
+        for (let i = pool.length - 1; i >= 0; i--) {
+          const box = document.createElement('div');
+          box.classList.add('box');
+          box.style.width = door.clientWidth + 'px';
+          box.style.height = door.clientHeight + 'px';
+          box.textContent = pool[i];
+          boxesClone.appendChild(box);
+        }
+        boxesClone.style.transitionDuration = `${duration > 0 ? duration : 1}s`;
+        boxesClone.style.transform = `translateY(-${door.clientHeight * (pool.length - 1)}px)`;
+        door.replaceChild(boxesClone, boxes);
+      }
+    }
+  //spin function async
+    async function spin() {
+      init(false, 1, 2);
+      
+      for (const door of doors) {
+        const boxes = door.querySelector('.boxes');
+        const duration = parseInt(boxes.style.transitionDuration);
+        boxes.style.transform = 'translateY(0)';
+        await new Promise((resolve) => setTimeout(resolve, duration * 100));
+      }
+    }
+  // uuhhhh shuffle arrrr shuffleeee
+    function shuffle([...arr]) {
+      let m = arr.length;
+      while (m) {
+        const i = Math.floor(Math.random() * m--);
+        [arr[m], arr[i]] = [arr[i], arr[m]];
+      }
+      return arr;
+    }
+  
+    init();
+  })();
+  
+  let shuffles = document.getElementById("spinner")
+  let tekst = document.getElementById("tekst")
+
+shuffles.addEventListener(
+    function items(){
+        if(result[0] == result[1] == result[1] ){
+            tekst.innerHTML = "WOW DU VANDT";
+        }
+        else{
+            tekst.innerHTML = "du tabte LOL";
+        }
+    }
+    )
